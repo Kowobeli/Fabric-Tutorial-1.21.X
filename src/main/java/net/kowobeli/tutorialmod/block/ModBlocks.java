ab6a1a728d1ce41e00c2b74e2c192d267117cbf7
Pablo Silva Franco <owonekyone591@gmail.com>
2024-09-21 17:59:09 -0300

    Created four custom blocks and two custom items, complete with recipes, loot tables, and item groups

--- src/main/java/net/kowobeli/tutorialmod/block/ModBlocks.java
+++ src/main/java/net/kowobeli/tutorialmod/block/ModBlocks.java
@@ -4,6 +4,8 @@ import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.kowobeli.tutorialmod.TutorialMod;
 import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.Block;
+import net.minecraft.block.ExperienceDroppingBlock;
+import net.minecraft.client.gl.Uniform;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroups;
@@ -11,6 +13,7 @@ import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.sound.BlockSoundGroup;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.math.intprovider.UniformIntProvider;
 
 public class ModBlocks {
 
@@ -21,6 +24,15 @@ public class ModBlocks {
     public static final Block RAW_PINK_GARNET_BLOCK = registerBLock("raw_pink_garnet_block",
             new Block(AbstractBlock.Settings.create().strength(3f)
                     .requiresTool().sounds(BlockSoundGroup.AMETHYST_BLOCK)));
+
+
+    public static final Block PINK_GARNET_ORE = registerBLock("pink_garnet_ore",
+            new ExperienceDroppingBlock(UniformIntProvider.create(2, 5),
+                AbstractBlock.Settings.create().strength(3f).requiresTool()));
+
+    public static final Block PINK_GARNET_DEEPSLATE_ORE = registerBLock("pink_garnet_deepslate_ore",
+            new ExperienceDroppingBlock(UniformIntProvider.create(3, 6),
+                    AbstractBlock.Settings.create().strength(4f).requiresTool().sounds(BlockSoundGroup.DEEPSLATE)));
 
     private static Block registerBLock(String name, Block block){
         registerBLockItem(name, block);
@@ -37,6 +49,8 @@ public class ModBlocks {
         ItemGroupEvents.modifyEntriesEvent(ItemGroups.BUILDING_BLOCKS).register(entries -> {
             entries.add(ModBlocks.PINK_GARNET_BLOCK);
             entries.add(ModBlocks.RAW_PINK_GARNET_BLOCK);
+            entries.add(ModBlocks.PINK_GARNET_ORE);
+            entries.add(ModBlocks.PINK_GARNET_DEEPSLATE_ORE);
         });
     }
 }
--- src/main/java/net/kowobeli/tutorialmod/item/ModItems.java
+++ src/main/java/net/kowobeli/tutorialmod/item/ModItems.java
@@ -2,6 +2,7 @@ package net.kowobeli.tutorialmod.item;
 
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.kowobeli.tutorialmod.TutorialMod;
+import net.kowobeli.tutorialmod.block.ModBlocks;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroups;
 import net.minecraft.registry.Registries;
@@ -11,6 +12,8 @@ import net.minecraft.util.Identifier;
 public class ModItems {
     public static final Item PINK_GARNET = registerItem("pink_garnet", new Item(new Item.Settings()));
     public static final Item RAW_PINK_GARNET = registerItem("raw_pink_garnet", new Item(new Item.Settings()));
+    public static final Item PINK_GARNET_ORE = registerItem("raw_pink_garnet", new Item(new Item.Settings()));
+    public static final Item PINK_GARNET_DEEPSLATE_ORE = registerItem("raw_pink_garnet", new Item(new Item.Settings()));
 
     private static Item registerItem(String name, Item item){
         return Registry.register(Registries.ITEM, Identifier.of(TutorialMod.MOD_ID, name), item);
@@ -22,6 +25,8 @@ public class ModItems {
         ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(entries -> {
             entries.add(PINK_GARNET);
             entries.add(RAW_PINK_GARNET);
+            entries.add(PINK_GARNET_ORE);
+            entries.add(PINK_GARNET_DEEPSLATE_ORE);
         });
     }
 }
