53993bda944bc1d5666f391073fa9acf1b16acfb
Pablo Silva Franco <owonekyone591@gmail.com>
2024-09-22 20:51:32 -0300

    Added a custom item (chisel)

--- src/main/java/net/kowobeli/tutorialmod/item/custom/ChiselItem.java
+++ src/main/java/net/kowobeli/tutorialmod/item/custom/ChiselItem.java
@@ -3,10 +3,13 @@ package net.kowobeli.tutorialmod.item.custom;
 import net.kowobeli.tutorialmod.block.ModBlocks;
 import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
+import net.minecraft.client.particle.Particle;
+import net.minecraft.client.particle.ParticleManager;
 import net.minecraft.entity.EquipmentSlot;
 import net.minecraft.item.Equipment;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemUsageContext;
+import net.minecraft.particle.*;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.sound.SoundCategory;
@@ -25,7 +28,8 @@ public class ChiselItem extends Item {
                     Blocks.END_STONE, Blocks.END_STONE_BRICKS,
                     Blocks.OAK_LOG, ModBlocks.PINK_GARNET_BLOCK,
                     Blocks.GOLD_BLOCK, Blocks.NETHERITE_BLOCK,
-                    Blocks.DIRT, Blocks.DIAMOND_BLOCK
+                    Blocks.DIRT, Blocks.DIAMOND_BLOCK,
+                    Blocks.DIAMOND_BLOCK, Blocks.DIRT
             );
 
 
@@ -40,6 +44,7 @@ public class ChiselItem extends Item {
 
         if(CHISEL_MAP.containsKey(clickedBlock)){
             if(!world.isClient()){
+                var blockState = world.getBlockState(context.getBlockPos());
                 world.setBlockState(context.getBlockPos(), CHISEL_MAP.get(clickedBlock).getDefaultState());
 
                 context.getStack().damage(1, ((ServerWorld) world), ((ServerPlayerEntity) context.getPlayer()),
@@ -47,6 +52,12 @@ public class ChiselItem extends Item {
 
 
                 world.playSound(null, context.getBlockPos(), SoundEvents.BLOCK_GRINDSTONE_USE, SoundCategory.BLOCKS);
+                ((ServerWorld) world).spawnParticles(
+                        new BlockStateParticleEffect(ParticleTypes.BLOCK, blockState),
+                        context.getBlockPos().getX() + 0.5,
+                        context.getBlockPos().getY() + 1,
+                        context.getBlockPos().getZ() + 0.5,
+                        10, 0.5, 0.5, 0.5, 0.1);
             }
         }
 
